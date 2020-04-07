18b659e48602f08d46839b77bdeb396a1d809977
User <User@YD01-04>
2020-04-07 15:47:05 +0900

    commit

--- nadeulee/src/main/java/co/nadeulee/pjt/DAO/Example.java
+++ nadeulee/src/main/java/co/nadeulee/pjt/DAO/Example.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Example
  */
-@WebServlet("/Example")
+@WebServlet("/Example1")
 public class Example extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
