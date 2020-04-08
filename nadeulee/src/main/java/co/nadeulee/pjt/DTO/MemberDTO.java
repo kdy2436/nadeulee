ef276e06980374a7cb45e8a9534b1dc6d8addf16
User <User@YD01-01>
2020-04-08 17:35:19 +0900

    020408 찬진 중간 업로드

---
+++ nadeulee/src/main/java/co/nadeulee/pjt/DTO/MemberDTO.java
@@ -0,0 +1,64 @@
+package co.nadeulee.pjt.DTO;
+
+public class MemberDTO {
+	private String email;
+	private String pw;
+	private String gender;
+	private String profile;
+	private String auth;
+	private String nickname;
+	
+	public MemberDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
+}
